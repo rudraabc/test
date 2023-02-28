2594b07561a613ab8071a90bcd247edcb265c10d
rudraabc <akshaychaudhari813@gmail.com>
2023-02-28 16:33:39 +0530

    5 string

--- New.java
+++ New.java
@@ -2,4 +2,23 @@ class new
 {
     public static void main(String[] args)
     System.out.println("new branch");
+
+    Switch(3){
+        case 1:
+            System.out.println("caes 1");
+            break;
+        case 2:
+            System.out.println("caes 2");
+            break;
+        case 3:
+            System.out.println("caes 3");
+            break;
+        case 4:
+            System.out.println("caes 4");
+            break;
+        case 5:
+            System.out.println("caes 5");
+            break;
+        
+    }
 }
